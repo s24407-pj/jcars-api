a9641388d4c25cbe6c8f73045440c26d2af35600
Michal <s24407@pjwstk.edu.pl>
2023-11-28 20:05:05 +0100

    commit

--- src/main/java/edu/pjatk/jcarsapi/model/User.java
+++ src/main/java/edu/pjatk/jcarsapi/model/User.java
@@ -3,6 +3,7 @@ package edu.pjatk.jcarsapi.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "\"user\"")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
