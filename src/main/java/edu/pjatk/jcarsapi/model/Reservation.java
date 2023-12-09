15414ad1abf113a1b4cf713044bb478e53a44809
Michal <s24407@pjwstk.edu.pl>
2023-12-09 21:55:59 +0100

    add constructs

--- src/main/java/edu/pjatk/jcarsapi/model/Reservation.java
+++ src/main/java/edu/pjatk/jcarsapi/model/Reservation.java
@@ -28,6 +28,66 @@ public class Reservation {
     @Enumerated(EnumType.STRING)
     private ReservationStatus status;
 
+    public Reservation(Integer id, User user, Car car, LocalDateTime startDate, LocalDateTime endDate, ReservationStatus status) {
+        this.id = id;
+        this.user = user;
+        this.car = car;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public ReservationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ReservationStatus status) {
+        this.status = status;
+    }
+
+    public Reservation() {
+    }
+
     public enum ReservationStatus {
         ACTIVE, COMPLETED, CANCELED, PENDING_CONFIRMATION, PAID, APPROVED
     }
