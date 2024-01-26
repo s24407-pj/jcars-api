ea6fc83f72bc94c7d5e80a9096a6898d913b63fa
Dawid Janas <124461619+DawidJanas123@users.noreply.github.com>
2024-01-26 23:37:18 +0100

    dodanie rol do pojosa

--- src/main/java/edu/pjatk/jcarsapi/controller/AuthController.java
+++ src/main/java/edu/pjatk/jcarsapi/controller/AuthController.java
@@ -32,7 +32,7 @@ import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/jcars")
 public class AuthController {
 
     private final UserRepository userRepository;
